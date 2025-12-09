4667bfb7326f93d51ed0bebc02977641593f8524
Sana Hashmat <sanahashmat14@gmail.com>
2025-12-09 19:20:04 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,36 @@
+//to check number is even or oddd using if else
+#include<stdio.h>
+int main()
+{
+   int x;
+   printf("Enter a number: ");
+   scanf("%d", &x);
+
+   if(x%2==0){
+    printf("%d is even\n", x);
+
+   }
+   else{
+    printf("%d is odd\n", x);
+   }
+   return 0;
+}
+
+
+//using if
+#include<stdio.h>
+int main()
+{
+int x;
+printf("Enter a number:");
+scanf("%d",&x);
+if(x%2==0)
+{
+printf("%d is even\n",x);
+}
+  if(x%2!=0)
+{
+printf("%d is odd\n",x);
+}
+return 0;
+}
